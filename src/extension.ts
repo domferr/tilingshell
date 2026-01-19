582cda18acfc880f2531bd0bd0ae6e28f6fee3c0
George Thomas <georgefsthomas@gmail.com>
2026-01-19 18:47:31 +0000

    Avoid `indexOf`

--- src/extension.ts
+++ src/extension.ts
@@ -633,8 +633,9 @@ export default class TilingShellExtension extends Extension {
         const stackingOrder =
             global.display.sort_windows_by_stacking(windowList);
 
-        windowList
-            .filter((win) => {
+        stackingOrder
+            .map((win, winStackIndex) => ({ win, winStackIndex }))
+            .filter(({ win }) => {
                 if (win === focus_window || win.minimized) return false;
                 if (
                     onlyTiledWindows &&
@@ -655,7 +656,7 @@ export default class TilingShellExtension extends Extension {
                 }
                 return false;
             })
-            .forEach((win) => {
+            .forEach(({ win, winStackIndex }) => {
                 const winRect = win.get_frame_rect();
                 const winCenter = {
                     x: winRect.x + winRect.width / 2,
@@ -666,8 +667,6 @@ export default class TilingShellExtension extends Extension {
                     winCenter,
                     focusWindowCenter,
                 );
-
-                const winStackIndex = stackingOrder.indexOf(win);
 
                 if (
                     !bestWindow ||
